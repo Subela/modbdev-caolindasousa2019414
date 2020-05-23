c7e375a9d2691865e475c6578810916a068cc496
Subela <olindasuely@yahoo.com.br>
2020-05-23 21:57:18 +0000

    Implement Camera function

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,8 +4,7 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [{
     path: '',
     loadChildren: () => import('./pages/tabs/tabs.module').then(m => m.TabsPageModule)
-},]    
-;
+},];
 
 @NgModule({
     imports: [
